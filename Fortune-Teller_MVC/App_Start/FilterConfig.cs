d90693bebe8f57419474afec780e00b37757751d
Alexis Chandler <alexiscchandler@gmail.com>
2016-10-21 19:01:00 -0400

    changed everything back

--- Fortune-Teller_MVC/App_Start/FilterConfig.cs
+++ Fortune-Teller_MVC/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Fortune_Teller_MVC
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            //filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleErrorAttribute());
         }
     }
 }
